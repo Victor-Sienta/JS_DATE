797fd89cae75e9cd98ee1b9aee4710f84bbab511
Бакотин <bakotin@tmktools.ru>
2023-11-07 11:08:19 +0300

    Обновление задачи 3

--- js/3.js
+++ js/3.js
@@ -1,5 +1,20 @@
 function task1(){
     let den= ["воскресенье","понедельник", "вторник", "среда","четверг", "пятница", "суббота"];
     let dat=new Date();
-    alert(`Вчера: ${den[dat.getDay()-1]} \nСегодня: ${den[dat.getDay()]} \nЗавтра: ${den[dat.getDay()+1]}`)
+    let today = dat.getDay();
+    function lastDay(){
+        if (today == 0)
+            return 6;
+        else{
+            return (today-1)
+        }
+    }
+    function tomorrowDay(){
+        if (today == 6)
+            return 0;
+        else{
+            return (today+1)
+        }
+    }
+    alert(`Вчера: ${den[lastDay(today)]} \nСегодня: ${den[dat.getDay()]} \nЗавтра: ${den[tomorrowDay(today)]}`)
 }
